b3c86a710b79230acf6e1d6959415fda379b71ad
31foan1dpt <150041658+31foan1dpt@users.noreply.github.com>
2023-12-23 14:34:52 +0100

    first commit.
    created Backend Project

---
+++ MiniJira-Service/src/main/java/com/example/MiniJiraService/MiniJiraServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniJiraService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniJiraServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniJiraServiceApplication.class, args);
+	}
+
+}
---
+++ MiniJira-Service/src/test/java/com/example/MiniJiraService/MiniJiraServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniJiraService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniJiraServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
