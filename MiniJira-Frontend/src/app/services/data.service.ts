41848c7b1fb07a7d6b4bcd7bf5162d954592348c
forte <antonino.forte@adesso.de>
2023-12-25 11:40:08 +0100

    create a dataService to fetch data from BE

---
+++ MiniJira-Frontend/src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { env } from '../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+  private url = env.apiUrl;
+  private mapping = env.mapping;
+
+  constructor(private http: HttpClient) { }
+
+  getAllTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.url}${this.mapping}`)
+  }
+
+  getTaskById(id: String): Observable<Task> {
+    return this.http.get<Task>(`${this.url}${this.mapping}/${id}`)
+  }
+
+  createTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(`${this.url}${this.mapping}`, task);
+  }
+
+  deleteTask(id: String): Observable<Task> {
+    return this.http.delete<Task>(`${this.url}${this.mapping}/${id}`)
+  }
+}
